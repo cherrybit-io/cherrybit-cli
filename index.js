3509c668e1690737f80e74fc3730147217fa1b3f
Pablo Jose <josepbl808@gmail.com>
2020-01-29 16:54:16 -0600

    create command args wallet create

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const colors = require('colors');
 const clear = require('clear');
 const CFonts = require('cfonts');
 
@@ -8,5 +7,25 @@ clear();
 const args = process.argv.slice(2);
 const appName = 'CherryBit';
 if (args.length === 0) {
-    CFonts.say(appName, {colors: ['red']});
+    CFonts.say(appName, {colors: ['#ff5d49']});
+    console.log(`Usage: 
+    ${appName} balance
+        gets balance in tokens and shows public addres
+
+    ${appName} send <amount> <address>
+        Send a fixed amount of tokens from your wallet to the specified address
+
+    ${appName} wallet create
+        Guide you through creating a new wallet`);
+    process.exit();
 }
+
+const main = async () => {
+    if (args[0] === 'wallet') {
+        if (args[1] === 'create') {
+            console.log('Guides you through creating a new wallet');
+        }
+    }
+};
+
+main();
