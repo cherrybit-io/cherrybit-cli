08004dd5f7a8e1ea822ab42bb2f48c8d092b5f0b
Pablo Jose <josepbl808@gmail.com>
2020-01-29 17:03:57 -0600

    set a flow for cherrybit commands

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ clear();
 const args = process.argv.slice(2);
 const appName = 'CherryBit';
 if (args.length === 0) {
-    CFonts.say(appName, {colors: ['#ff5d49']});
+    CFonts.say(appName, {colors: ['red']});
     console.log(`Usage: 
     ${appName} balance
         gets balance in tokens and shows public addres
@@ -21,10 +21,18 @@ if (args.length === 0) {
 }
 
 const main = async () => {
+    CFonts.say(appName, {colors: ['red']});
     if (args[0] === 'wallet') {
         if (args[1] === 'create') {
             console.log('Guides you through creating a new wallet');
         }
+    } else if (args[0] === 'balance') {
+        console.log('Gets balance in tokens and shows public address');
+    } else if (args[0] === 'send') {
+        console.log('Send a fixed amount of tokens from your wallet to the specified address');
+    } else {
+        console.log('That was not a valid command. Please try again.');
+        process.exit();
     }
 };
 
